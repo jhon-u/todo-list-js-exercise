cf86af6397fead925879b6dfb46a5680957cd9c3
Jhon Useche <jhon.useche@telus.com>
2022-11-24 20:05:11 -0700

    Add functions to NewTask object to be used as methods

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = function(title, description) {
   const task = {
     title,
     description,
-    complete: false
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has ${this.complete ? "" : "not"} been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
+
+
   return task;
-}
+};
 
 //Prints out the provided task's details
-function logTaskState(task) {
-  console.log(`${task.title} has ${task.complete ? "" : "not"} been completed`);
-}
+
 
 // Marks the provided task as completed
-function completeTask(task) {
-  task.complete = true;
-}
+
 
 
 
@@ -27,8 +33,8 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1);
-completeTask(task1);
-logTaskState(task1);
+task1.logState();
+task1.markCompleted();
+task1.logState();
 
 console.log(tasks);
